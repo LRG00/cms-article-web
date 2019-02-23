398bb5ee95f721c3e938c68a288ede62ba30c403
LRG00 <158097628@qq.com>
2019-02-23 11:53:35 +0800

    查询接口优化

--- server/src/controller/article.js
+++ server/src/controller/article.js
@@ -15,6 +15,7 @@ router.get('/bar', function (ctx, next) {
 
 // 增
 router.post('/add', async (ctx) => {
+  console.log(ctx.request.body, '9299');
   const person = new Person({
     title: ctx.request.body.title,
     author: ctx.request.body.author,
@@ -34,12 +35,13 @@ router.post('/add', async (ctx) => {
   };
 });
 // 查
-router.post('/article-list', async (ctx) => {
-  console.log(ctx.request.body, '9299');
-  const title = await Person.findOne({ title: ctx.request.body.title });
+router.post('/article-list', async function(ctx) {
+  // const result = await Person.findOne({name: ctx.request.body.name})
+  const results = await Person.find({name: ctx.request.body.name})
   ctx.body = {
     code: 0,
-    title,
+    // result,
+    results
   }
 })
 module.exports = router;
