3b95323d9a8594542822431abab244d006febdd0
LRG00 <158097628@qq.com>
2019-06-21 14:06:28 +0800

    dev

--- server/src/app.js
+++ server/src/app.js
@@ -1,46 +1,41 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const koajwt = require('koa-jwt');
-const cors = require('@koa/cors');
-const bodyParser = require('koa-bodyparser');
-const helmet = require('koa-helmet');
-const respond = require('koa-respond');
-const logger = require('koa-logger');
-const serve = require('koa-static');
-const path = require('path');
+const Koa = require("koa");
+const Router = require("koa-router");
+const koajwt = require("koa-jwt");
+const cors = require("@koa/cors");
+const bodyParser = require("koa-bodyparser");
+const helmet = require("koa-helmet");
+const respond = require("koa-respond");
+const logger = require("koa-logger");
+const serve = require("koa-static");
+const path = require("path");
 
-const article = require('./controller/article');
-const cate = require('./controller/cate');
-const users = require('./controller/users');
-const login = require('./controller/login');
-const mongoose = require('mongoose');
-// 连接mongoose
-const DB_URL = 'mongodb://leeruigan:as123456@ds119702.mlab.com:19702/blog';
-mongoose.connect(DB_URL);
-mongoose.connection.on('connected', () => console.log('已启动mongo'));
+const api = require("./routes/api");
+const login = require("./routes/login");
 
 const app = new Koa();
 const router = new Router();
 const port = process.env.PORT || 3001;
 
-require('./router')(router);
+require("./router")(router);
 // 错误处理
 app.use((ctx, next) => {
-  return next().catch((err) => {
-      if(err.status === 401){
-          ctx.status = 401;
-        ctx.body = 'Protected resource, use Authorization header to get access\n';
-      }else{
-          throw err;
-      }
-  })
-})
+  return next().catch(err => {
+    if (err.status === 401) {
+      ctx.status = 401;
+      ctx.body = "Protected resource, use Authorization header to get access\n";
+    } else {
+      throw err;
+    }
+  });
+});
 
-app.use(koajwt({
-secret: 'my_token'
-}).unless({
-path: [/login/]
-}));
+app.use(
+  koajwt({
+    secret: "my_token"
+  }).unless({
+    path: [/login/]
+  })
+);
 
 app
   .use(cors())
@@ -50,14 +45,10 @@ app
   .use(respond())
   .use(router.routes())
   .use(router.allowedMethods())
-  .use(article.routes(), article.allowedMethods())
   .use(login.routes(), login.allowedMethods())
-  .use(users.routes(), users.allowedMethods())
-  .use(cate.routes(), cate.allowedMethods())
-  .use(serve(path.join(process.cwd(), '../client/build')))
+  .use(api.routes(), api.allowedMethods())
+  .use(serve(path.join(process.cwd(), "../client/build")))
   .listen(port, () => {
-    console.log('The server is running at:');
-    console.log(
-      `    - Local:  http://localhost:${port}`
-    );
+    console.log("The server is running at:");
+    console.log(`    - Local:  http://localhost:${port}`);
   });
